4e3a17deb1201f3715e9572aa06200f1a65b9c49
= <=>
2025-05-21 19:08:52 +0700

    program tugas akhir website

--- page/workshop.js
+++ page/workshop.js
@@ -1,3 +1,14 @@
+// fetch('workshop.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     const item = data[2];
+
+//     document.getElementById('foto').src = item.foto;
+//     document.getElementById('judul').textContent = item.judul;
+//     document.getElementById('isi').innerHTML = item.isi;
+//   })
+//   .catch(error => console.error('Gagal memuat data:', error));
+
 fetch('workshop.json')
   .then(response => response.json())
   .then(data => {
