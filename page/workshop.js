80572e470d597cc5da9fbee31dc1bba7b032d6ec
= <=>
2025-05-21 19:07:19 +0700

    .\.git

--- page/workshop.js
+++ page/workshop.js
@@ -1,30 +1,17 @@
-// fetch('workshop.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     const item = data[2];
-
-//     document.getElementById('foto').src = item.foto;
-//     document.getElementById('judul').textContent = item.judul;
-//     document.getElementById('isi').innerHTML = item.isi;
-//   })
-//   .catch(error => console.error('Gagal memuat data:', error));
-
 fetch('workshop.json')
   .then(response => response.json())
   .then(data => {
     const container = document.getElementById('workshop-container');
 
     data.forEach(item => {
-      // Buat elemen wrapper
+
       const contentDiv = document.createElement('div');
       contentDiv.className = 'content';
 
-      // Buat elemen gambar
       const img = document.createElement('img');
       img.src = item.foto;
       img.alt = item.judul;
 
-      // Buat elemen teks
       const textDiv = document.createElement('div');
       textDiv.className = 'workshoptext';
 
@@ -32,16 +19,14 @@ fetch('workshop.json')
       title.textContent = item.judul;
 
       const desc = document.createElement('p');
-      desc.innerHTML = item.isi; // gunakan innerHTML karena ada <br>
+      desc.innerHTML = item.isi;
 
-      // Susun struktur elemen
       textDiv.appendChild(title);
       textDiv.appendChild(desc);
 
       contentDiv.appendChild(img);
       contentDiv.appendChild(textDiv);
 
-      // Tambahkan ke container
       container.appendChild(contentDiv);
     });
   })
